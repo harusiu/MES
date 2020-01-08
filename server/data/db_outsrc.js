2efa6cbf37aa85be9b175a827c1cc8af15038208
mingzoo <wu.mingzoo@gmail.com>
2020-01-08 17:56:40 +0800

    Update db_outsrc.js

--- server/data/db_outsrc.js
+++ server/data/db_outsrc.js
@@ -1,9 +1,6 @@
 module.exports = {
 	mysql: {
-		//host: '127.0.0.1',
-        host: '192.168.1.231',
-		//user: 'fortop_outsrc',
-		//password: 'iIiforToP@outSrcpsw0rd',
+        	host: 'localhost',
 		user: 'fortop_mes',
 		password: 'iIiforToP@Mespsw0rd',
 		database: 'fortop_outsrc',
