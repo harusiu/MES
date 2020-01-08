fe4f887d4b4f6c93480a5817b9046319c21acb9d
mingzoo <wu.mingzoo@gmail.com>
2020-01-08 17:58:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import router from './router'
 import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from './assets/icons/icons.js'
 
+import axios from 'axios'                   //Mingzoo
+import VueAxios from 'vue-axios'            //Mingzoo
+import BootstrapVue from 'bootstrap-vue'    //Mingzoo
+
 Vue.config.performance = true
 Vue.use(CoreuiVue)
+
+Vue.use(VueAxios, axios)    //Mingzoo
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
