dc5880b5986e9d381e80d7d5610a06cae74850d0
mingzoo <wu.mingzoo@gmail.com>
2020-01-08 17:56:07 +0800

    Update db_hr.js

--- server/data/db_hr.js
+++ server/data/db_hr.js
@@ -2,7 +2,7 @@
 // MySQL資料庫連接配置
 module.exports = {
 	mysql: {
-		host: '192.168.1.231',
+		host: 'localhost',
 		user: 'fortop_mes',
 		password: 'iIiforToP@Mespsw0rd',
 		database: 'human_resource',
